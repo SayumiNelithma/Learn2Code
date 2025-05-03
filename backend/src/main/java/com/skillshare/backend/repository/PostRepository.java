362338c7e74e39d01950c53ee9cddd867ee54ae8
Nethma Gunawardana <nipunigunawardana777@gmail.com>
2025-05-03 12:31:42 +0530

    import headers in PostRepository

--- backend/src/main/java/com/skillshare/backend/repository/PostRepository.java
+++ backend/src/main/java/com/skillshare/backend/repository/PostRepository.java
@@ -1,5 +1,8 @@
 package com.skillshare.backend.repository;
 
-public class PostRepository {
-    
-}
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skillshare.backend.entity.Post;
+import com.skillshare.backend.entity.User;
