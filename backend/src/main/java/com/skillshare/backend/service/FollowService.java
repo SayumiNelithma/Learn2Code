f9040d09f5d1d1258269c93f0c9b858b96826053
sayuminelithma@gmail.com <156690349+SayumiNelithma@users.noreply.github.com>
2025-05-03 12:43:17 +0530

    add imports in FollowService.java

--- backend/src/main/java/com/skillshare/backend/service/FollowService.java
+++ backend/src/main/java/com/skillshare/backend/service/FollowService.java
@@ -1,5 +1,11 @@
 package com.skillshare.backend.service;
 
-public class FollowService {
-    
-}
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skillshare.backend.entity.Follow;
+import com.skillshare.backend.entity.FollowStatus;
+import com.skillshare.backend.entity.User;
+import com.skillshare.backend.repository.FollowRepository;
