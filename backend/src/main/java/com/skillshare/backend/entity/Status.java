d5509a192bb51ba38d2574bcda08858104a6c34a
sayuminelithma@gmail.com <156690349+SayumiNelithma@users.noreply.github.com>
2025-05-03 12:38:44 +0530

    import headers Status.java

--- backend/src/main/java/com/skillshare/backend/entity/Status.java
+++ backend/src/main/java/com/skillshare/backend/entity/Status.java
@@ -1,5 +1,16 @@
 package com.skillshare.backend.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
 public class Status {
     
 }
