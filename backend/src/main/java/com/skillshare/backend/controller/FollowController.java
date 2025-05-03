8c9413b9d3aca1ec60442a6ae9b032efdc1ae0e0
sayuminelithma@gmail.com <156690349+SayumiNelithma@users.noreply.github.com>
2025-05-03 12:33:21 +0530

    import headers in FollowController.java

--- backend/src/main/java/com/skillshare/backend/controller/FollowController.java
+++ backend/src/main/java/com/skillshare/backend/controller/FollowController.java
@@ -1,5 +1,20 @@
 package com.skillshare.backend.controller;
 
-public class FollowController {
-    
-}
+import java.security.Principal;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skillshare.backend.entity.Follow;
+import com.skillshare.backend.entity.FollowStatus;
+import com.skillshare.backend.entity.User;
+import com.skillshare.backend.repository.UserRepository;
+import com.skillshare.backend.service.FollowService;
