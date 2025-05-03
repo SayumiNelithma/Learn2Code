8663cf3d427231aa52a33cfe68475cc48cd79449
Navodhya2 <vidanagagamagesunil@gmail.com>
2025-05-03 12:38:02 +0530

    Add import statements

--- backend/src/main/java/com/skillshare/backend/controller/LearningPlanController.java
+++ backend/src/main/java/com/skillshare/backend/controller/LearningPlanController.java
@@ -1,4 +1,20 @@
 package com.skillshare.backend.controller;
+import java.security.Principal;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skillshare.backend.entity.LearningPlan;
+import com.skillshare.backend.entity.User;
+import com.skillshare.backend.repository.UserRepository;
+import com.skillshare.backend.service.LearningPlanService;
+
 
 public class LearningPlanController {
     
