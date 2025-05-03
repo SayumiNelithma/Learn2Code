8f8d9a89f7cf35e7a48099226094fa422ed8409e
sayuminelithma@gmail.com <156690349+SayumiNelithma@users.noreply.github.com>
2025-05-03 12:37:05 +0530

    commit imports in Follow.java

--- backend/src/main/java/com/skillshare/backend/entity/Follow.java
+++ backend/src/main/java/com/skillshare/backend/entity/Follow.java
@@ -1,5 +1,11 @@
 package com.skillshare.backend.entity;
 
-public class Follow {
-    
-}
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
