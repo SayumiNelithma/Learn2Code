2b99abd2f2cff22f87cf2d15343949ba471aac89
sayuminelithma@gmail.com <156690349+SayumiNelithma@users.noreply.github.com>
2025-05-03 12:35:31 +0530

    import headers in statusController.java

--- backend/src/main/java/com/skillshare/backend/controller/StatusController.java
+++ backend/src/main/java/com/skillshare/backend/controller/StatusController.java
@@ -1,5 +1,25 @@
 package com.skillshare.backend.controller;
 
-public class StatusController {
-    
-}
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.skillshare.backend.config.JwtUtil;
+import com.skillshare.backend.entity.Status;
+import com.skillshare.backend.entity.User;
+import com.skillshare.backend.repository.StatusRepository;
+import com.skillshare.backend.repository.UserRepository;
