51f502fb72c152aa863d4941edff2b06ce3da081
Nethma Gunawardana <nipunigunawardana777@gmail.com>
2025-05-03 12:29:46 +0530

    import post.java

--- backend/src/main/java/com/skillshare/backend/entity/post.java
+++ backend/src/main/java/com/skillshare/backend/entity/post.java
@@ -1,5 +1,18 @@
 package com.skillshare.backend.entity;
 
-public class post {
-    
-}
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
