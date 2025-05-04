173efe45c7f48d49b17d4d7f26e2e47f86a6e615
senura upesh <it22179180@my.sliit.lk>
2025-05-04 22:31:43 +0530

    Added comment.java

--- backend/src/main/java/com/skillshare/backend/entity/Comment.java
+++ backend/src/main/java/com/skillshare/backend/entity/Comment.java
@@ -1,5 +1,31 @@
 package com.skillshare.backend.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "comments")
 public class Comment {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Post post;
 }
+
