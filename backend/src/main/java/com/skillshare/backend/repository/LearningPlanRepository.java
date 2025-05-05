829027fd38f7398ff6d94136c5a8d115a7e79879
Navodhya2 <vidanagagamagesunil@gmail.com>
2025-05-05 20:08:13 +0530

    add import statements

--- backend/src/main/java/com/skillshare/backend/repository/LearningPlanRepository.java
+++ backend/src/main/java/com/skillshare/backend/repository/LearningPlanRepository.java
@@ -1,4 +1,10 @@
 package com.skillshare.backend.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skillshare.backend.entity.LearningPlan;
+import com.skillshare.backend.entity.User;
 
 public class LearningPlanRepository {
     
