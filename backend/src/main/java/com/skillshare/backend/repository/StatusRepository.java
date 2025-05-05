a4f6e996e2d3a0866cd86c9e4056727f21a58502
sayuminelithma@gmail.com <156690349+SayumiNelithma@users.noreply.github.com>
2025-05-05 13:05:06 +0530

    handle login of the posts status

--- backend/src/main/java/com/skillshare/backend/repository/StatusRepository.java
+++ backend/src/main/java/com/skillshare/backend/repository/StatusRepository.java
@@ -1,5 +1,14 @@
 package com.skillshare.backend.repository;
 
-public class StatusRepository {
-    
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skillshare.backend.entity.Status;
+import com.skillshare.backend.entity.User;
+
+public interface StatusRepository extends JpaRepository<Status, Long> {
+    List<Status> findByUser(User user);
+    List<Status> findByExpiresAtAfter(LocalDateTime now);
 }
