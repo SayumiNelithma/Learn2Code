283f9e0393683d4859097eed5ee84800d878ad85
Navodhya2 <vidanagagamagesunil@gmail.com>
2025-05-05 20:11:02 +0530

    add imports for learninplanservice.java

--- backend/src/main/java/com/skillshare/backend/service/LearningPlanService.java
+++ backend/src/main/java/com/skillshare/backend/service/LearningPlanService.java
@@ -1,5 +1,16 @@
 package com.skillshare.backend.service;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skillshare.backend.entity.LearningPlan;
+import com.skillshare.backend.entity.User;
+import com.skillshare.backend.repository.LearningPlanRepository;
+
+
 public class LearningPlanService {
     
 }
