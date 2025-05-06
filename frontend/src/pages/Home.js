7b73f275b1d6614965d7c48d65e4b4f073296c1b
sayuminelithma@gmail.com <156690349+SayumiNelithma@users.noreply.github.com>
2025-05-06 11:34:50 +0530

    import right side bar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
 import Leftsidebar from "../components/homepage/Leftsidebar";
+import RightSidebar from "../components/homepage/Rightsidebar";
 
 const Home = () => {
   return (
-    <Leftsidebar />
-  )
-}
+    <Box>
+      <Leftsidebar />
 
-export default Home
+      <RightSidebar
+        user={user}
+        allUsers={allUsers}
+        followStatus={followStatus}
+        handleFollowRequest={handleFollowRequest}
+        handleUnfollow={handleUnfollow}
+      />
+    </Box>
+  );
+};
+
+export default Home;
