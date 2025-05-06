fd0d3dfb02a7f8d09707a258944340408ba4a405
senura upesh <it22179180@my.sliit.lk>
2025-05-06 11:39:49 +0530

    motion,axios and useAuth header formattings

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,3 +1,6 @@
 import { useState } from "react";
 import { Grid, Box, Typography, TextField, Button } from "@mui/material";
 import { toast } from "react-toastify";
+import { motion } from "framer-motion";
+import axios from "../api/axiosConfig";
+import { useAuth } from "../auth/AuthContext";
